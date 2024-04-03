105efa212214d22bd92f8feef43fec9c64eedbf1
fleminjohnson <fjfleminjohnson@gmail.com>
2024-04-04 00:54:58 +0530

    LevelLoading Mechanism, added in GameManger

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,20 +7,34 @@ namespace CardMatchingGame
     public class GameManager : SingletonBehaviour<GameManager>
     {
         [SerializeField] private CanvasManagement canvasManagement;
+        [SerializeField] private List<Transform> levelPrefabList;
+        [SerializeField] private Transform gameplayCanvas;
 
         private int turnCount = 0;
         private int matchCount = 0;
+        private int currentActiveLevelCount = 0;
+        private Transform currentActiveLevel = null;
 
-        // Start is called before the first frame update
-        void Start()
+        public void PlayGame()
         {
-
+            canvasManagement.PlayGame();
+            LoadNextLevel();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void LoadNextLevel()
         {
+            if(currentActiveLevelCount <= levelPrefabList.Count)
+            {
+                if(currentActiveLevelCount > 0 & currentActiveLevel != null)
+                    Destroy(currentActiveLevel);
 
+                currentActiveLevel = Instantiate(levelPrefabList[currentActiveLevelCount], gameplayCanvas);
+                currentActiveLevelCount++;
+            }
+            else
+            {
+                Debug.Log("Level is larger than in LevelList");
+            }
         }
 
         public void MatchFound()
